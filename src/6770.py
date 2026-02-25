7983e4d848bf372bbb3fd39a7d92b662fb8ef68d
willyhuang <aa0981273968@gmail.com>
2026-02-25 16:37:35 +0800

    2次

---
+++ src/6770.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_stock_price(stock_id):
+    url = f"https://tw.stock.yahoo.com/quote/{stock_id}"
+    
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+    
+    
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+
+    # 取得目前股價（class 可能會變動）
+    price = soup.find("span", {"class": "Fz(32px)"})
+    print(f"{stock_id} 今日股價2: {price.text}")
+    if price:
+        print(f"{stock_id} 今日股價2: {price.text}")
+    else:
+        print("找不到股價資訊（可能網站結構改變）")
+
+if __name__ == "__main__":
+    get_stock_price("6770")
