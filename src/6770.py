bb93a553767859b96f6f3aba94b1dea2890e6d0c
willyhuang <aa0981273968@gmail.com>
2026-02-25 21:00:22 +0800

    在家變更

--- src/6770.py
+++ src/6770.py
@@ -15,9 +15,10 @@ def get_stock_price(stock_id):
 
     # 取得目前股價（class 可能會變動）
     price = soup.find("span", {"class": "Fz(32px)"})
-    print(f"{stock_id} 今日股價2: {price.text}")
+    print(f"{stock_id} 今日股價3: {price.text}")
     if price:
-        print(f"{stock_id} 今日股價2: {price.text}")
+        print(f"{stock_id} 今日股價: {price.text}")
+        
     else:
         print("找不到股價資訊（可能網站結構改變）")
 
